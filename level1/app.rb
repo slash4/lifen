cab9e540c8382b1e424a3e1d350e79ac1924ee0f
Alex <alexcentar@Mac-mini-de-Alex.local>
2019-07-05 16:06:14 +0200

    [level1][Constants. Raw Json Parsing]

--- level1/app.rb
+++ level1/app.rb
@@ -1,3 +1,26 @@
 require "json"
+BASE_PRICE = 0.10
+COLOR_PRICE = 0.18
+ADDITIONNAL_PAGE_PRICE = 0.07
+EXPRESS_PRICE = 0.6
 
-# votre code
+class Practitioner
+  attr_accessor :id, :first_name, :last_name, :express_delivery
+
+  def initialize(item)
+    item.each_pair{|k,v| self.send("#{k}=", v)}
+  end
+end
+class Communication
+  attr_accessor :id, :practitioner_id, :pages_number, :color, :sent_at
+
+  def initialize(item)
+    item.each_pair{|k,v| self.send("#{k}=", v)}
+  end
+end
+
+#Data parsing
+data_hash = JSON.parse(File.read('data.json'), :symbolize_names => true)
+practitioners_hash = data_hash[:practitioners].map { |item| [item[:id], Practitioner.new(item)] }.to_h
+communications_hash = data_hash[:communications].map { |item| [item[:id], Communication.new(item)] }.to_h
+
