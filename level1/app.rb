14daf716b49f08e50f978f18029e76ce3adb9cee
Alex <alexcentar@Mac-mini-de-Alex.local>
2019-07-05 17:55:32 +0200

    Logic and tests

--- level1/app.rb
+++ level1/app.rb
@@ -1,9 +1,16 @@
 require "json"
+require 'date'
+#constants
 BASE_PRICE = 0.10
 COLOR_PRICE = 0.18
 ADDITIONNAL_PAGE_PRICE = 0.07
 EXPRESS_PRICE = 0.6
 
+#variables
+@practitioners_hash = {}
+@communications_hash = {}
+
+#classes
 class Practitioner
   attr_accessor :id, :first_name, :last_name, :express_delivery
 
@@ -12,15 +19,46 @@ class Practitioner
   end
 end
 class Communication
-  attr_accessor :id, :practitioner_id, :pages_number, :color, :sent_at
+  attr_accessor :id, :practitioner_id, :pages_number, :color, :sent_at, :practitioner
 
-  def initialize(item)
+  def initialize(item, practitioners)
     item.each_pair{|k,v| self.send("#{k}=", v)}
+    @practitioner=practitioners[practitioner_id]
+    @sent_at=DateTime.parse(item[:sent_at])
   end
+  def calculate
+    result = BASE_PRICE
+    result += ((pages_number-1)*ADDITIONNAL_PAGE_PRICE)
+    result += (COLOR_PRICE) if color
+    result += (EXPRESS_PRICE) if practitioner.express_delivery
+    result
+  end
+
 end
 
-#Data parsing
-data_hash = JSON.parse(File.read('data.json'), :symbolize_names => true)
-practitioners_hash = data_hash[:practitioners].map { |item| [item[:id], Practitioner.new(item)] }.to_h
-communications_hash = data_hash[:communications].map { |item| [item[:id], Communication.new(item)] }.to_h
+def parse_data raw
+  @practitioners_hash = raw[:practitioners].map { |item| [item[:id], Practitioner.new(item)] }.to_h
+  @communications_hash = raw[:communications].map { |item| [item[:id], Communication.new(item, @practitioners_hash)] }.to_h
+end
 
+def prices_by_day
+  result = {
+    totals: []
+  }
+  sum = {}
+  @communications_hash.values.each do |comm|
+    day = comm.sent_at.to_date.to_s
+    sum[day] ||= 0
+    sum[day] += comm.calculate
+  end
+  sum.each_pair do |k,v|
+    result[:totals] << {sent_on: k, total: v.round(2)}
+  end
+  result
+end
+
+#Main code
+data_hash = JSON.parse(File.read('data.json'), :symbolize_names => true)
+parse_data(data_hash)
+
+puts prices_by_day.inspect
--- level1/app_spec.rb
+++ level1/app_spec.rb
@@ -5,8 +5,45 @@ require 'rspec'
 require_relative './app.rb'
 
 RSpec.describe "Prices calculator", type: :model do
-  let(:data)          { JSON.parse(File.read('data.json')) }
-  let(:output)        { JSON.parse(File.read('output.json')) }
+  let(:data)          { JSON.parse(File.read('data.json'), :symbolize_names => true) }
+  let(:output)        { JSON.parse(File.read('output.json'), :symbolize_names => true) }
 
-  pending("calculates prices")
+  it "parses json" do
+    parse_data(data)
+    expect(@practitioners_hash.values.count).to eq 4
+    expect(@practitioners_hash[1].first_name).to eq "Gregory"
+  end
+  it "calculates prices" do
+    c = Communication.new(
+      {
+        id: 1,
+        practitioner_id: 1,
+        pages_number: 2,
+        color: true,
+        sent_at: "2019-01-01"
+      },
+      {
+        1=> Practitioner.new(
+          {
+            id: 1,
+            first_name: 'test',
+            last_name: 'test',
+            express_delivery: true
+          }
+        )
+      }
+    )
+
+    expect(c.calculate).to eq 0.95
+
+    c.color = false
+    expect(c.calculate).to eq 0.77
+
+    c.practitioner.express_delivery = false
+    expect(c.calculate).to eq 0.17
+  end
+  it "generates correct json" do
+    parse_data(data)
+    expect(prices_by_day).to eq output
+  end
 end
